acd354fba19a3142cae7337933310bd67ee3a220
Glitch (as91897v1) <none>
2020-09-01 00:29:22 +0000

    Changed a few things (forgot to update)

--- ua.js
+++ ua.js
@@ -2,3 +2,21 @@ var b = document.documentElement;
               b.setAttribute('data-useragent',  navigator.userAgent);
               b.setAttribute('data-platform', navigator.platform );
               b.className += ((!!('ontouchstart' in window) || !!('onmsgesturechange' in window))?' touch':'');
+
+
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("myTopnav");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
